e7cf554c07bc378335adbb7bd1c2c5c4f1ad21ad
Kipokape <karandash022@gmail.com>
2023-06-26 23:28:35 +0300

    GET-запрос для получения случайного аккаунта

--- bankAccountGenerator/src/main/java/com/example/bankAccountGenerator/BankAccountGeneratorController.java
+++ bankAccountGenerator/src/main/java/com/example/bankAccountGenerator/BankAccountGeneratorController.java
@@ -1,7 +1,20 @@
 package com.example.bankAccountGenerator;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.bankAccountGenerator.model.BankAccount;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/bankAccountGenerator")
+@AllArgsConstructor
 public class BankAccountGeneratorController {
+
+    private final BankAccountGeneratorService service;
+
+    @GetMapping("/")
+    public BankAccount getRandomGeneratedAccount(){
+        return service.generateRandomAccount();
+    }
+
+
 }
