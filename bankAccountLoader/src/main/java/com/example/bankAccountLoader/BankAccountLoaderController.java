99449c91d607c4e35e826b5c37a2af67cfe06ebd
Kipokape <karandash022@gmail.com>
2023-06-27 02:03:11 +0300

    Сохранение получаемого аккаунта в БД

--- bankAccountLoader/src/main/java/com/example/bankAccountLoader/BankAccountLoaderController.java
+++ bankAccountLoader/src/main/java/com/example/bankAccountLoader/BankAccountLoaderController.java
@@ -1,11 +1,41 @@
 package com.example.bankAccountLoader;
 
+import com.example.bankAccountLoader.model.BankAccount;
+import com.example.bankAccountLoader.model.TechResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/bankAccountLoader")
 @AllArgsConstructor
 public class BankAccountLoaderController {
+
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private BankAccountLoaderService service;
+
+    public BankAccountLoaderController() {
+        this.restTemplate = new RestTemplate();
+    }
+
+    @GetMapping("/")
+    public TechResponse saveBankAccount(){
+        try {
+            String url = "http://localhost:8080/bankAccountGenerator/";
+            ResponseEntity<BankAccount> response = restTemplate.getForEntity(url, BankAccount.class);
+            return service.saveAccount(response.getBody());
+        }catch (Exception ex){
+            return new TechResponse("ERROR","Ошибка получения данных от сервиса генерации аккаунтов");
+        }
+
+
+    }
+
+
 }
--- bankAccountLoader/src/main/java/com/example/bankAccountLoader/BankAccountLoaderService.java
+++ bankAccountLoader/src/main/java/com/example/bankAccountLoader/BankAccountLoaderService.java
@@ -1,8 +1,29 @@
 package com.example.bankAccountLoader;
 
 
+import com.example.bankAccountLoader.model.BankAccount;
+import com.example.bankAccountLoader.model.TechResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BankAccountLoaderService {
+
+    @Autowired
+    BankAccountRepository repository;
+
+
+    public TechResponse saveAccount(BankAccount account){
+
+        if(findAccount(account)){
+            return new TechResponse("ERROR","Аккаунт уже зарегистрирован в БД");
+        }else {
+            repository.save(account);
+        }
+        return new TechResponse("SUCCESS","null");
+    }
+
+    private boolean findAccount(BankAccount account){
+        return repository.findById(account.getUuid()).isPresent() || repository.findByAccountNumber(account.getAccountNumber()).isPresent();
+    }
 }
