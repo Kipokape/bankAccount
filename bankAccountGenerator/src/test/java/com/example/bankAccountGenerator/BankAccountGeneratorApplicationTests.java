2a2104e1874b461cbf964066798e3037a4477c2c
Kipokape <karandash022@gmail.com>
2023-06-26 20:59:12 +0300

    Создание первого проекта генератора

---
+++ bankAccountGenerator/src/test/java/com/example/bankAccountGenerator/BankAccountGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankAccountGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
