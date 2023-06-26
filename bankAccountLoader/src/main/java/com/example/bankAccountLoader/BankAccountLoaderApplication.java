17e387384655092484f68f94317854dc74e0ab51
Kipokape <karandash022@gmail.com>
2023-06-26 23:47:59 +0300

    Создание проекта для сервиса сохранения аккаунтов

---
+++ bankAccountLoader/src/main/java/com/example/bankAccountLoader/BankAccountLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankAccountLoader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountLoaderApplication.class, args);
+	}
+
+}
---
+++ bankAccountLoader/src/test/java/com/example/bankAccountLoader/BankAccountLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankAccountLoader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
